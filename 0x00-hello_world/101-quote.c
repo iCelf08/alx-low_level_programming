a53dc29cf201f18cfb7713cfdfc796927f5c2caa
chai <chfirstly@gmail.com>
2023-08-11 02:54:45 +0100

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - program prints a line to standart erroe
- * Return 1 (success)
+ * main - program prints a line to standart
+ *Return: 1 (success)
  */
 int main(void)
 {
-fputs("\"and that piece of art is useful\" - DoraKorpar, 2015-10-19\n", stdout);
+fputs("and that piece of art is useful\" - DoraKorpar, 2015-10-19\n", stdout);
 return (1);
 }
