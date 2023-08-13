3769b4f0c2652e194753b54124ec03766f79af16
ch <chfirslty@gmail.com>
2023-08-13 15:02:20 -0700

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+*main- program that printf combination of single number
+*Return:0 (success)
+*/
+int main(void)
+{
+int n;
+for (n = 0; n < 10; n++)
+{
+putchar('0' + n);
+if (n != 9)
+{
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+return (0);
+}
