f479f68b9ba10d32ad627cee43256b262e0a4540
ch <chfirslty@gmail.com>
2023-08-13 13:52:21 -0700

    8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 /**
- *main- program that printf numbers in hexadeimal in lower case
- *Return:0 (success)
- */
+*main- program that printf numbers in hexadeimal in lower case
+*Return:0 (success)
+*/
 int main(void)
-{int n;
+{
+int n;
 for (n = 0; n < 16; n++)
 {
 if (n < 10)
@@ -14,6 +15,7 @@ putchar('0' + n);
 else
 {
 putchar('a' + (n - 10));
+}
 }
 putchar('\n');
 return (0);
