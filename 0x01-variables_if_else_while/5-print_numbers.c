d83ec55d262038e62af002bfb9e90718bed07690
ch <chfirslty@gmail.com>
2023-08-13 11:23:09 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+*main-program that prints single numbers from 0 to 9
+*Return: 0 (sucsses)
+*/
+int main(void)
+{
+char n;
+for (n = 0; n < 10 ; n++)
+putchar(n);
+putchar('\n');
+return (0);
+}
