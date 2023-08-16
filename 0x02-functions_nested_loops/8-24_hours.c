e9e053d7b2d5b03d8c3575a57bb76448ac4cfea4
ch <chfirslty@gmail.com>
2023-08-16 08:45:04 -0700

    8-24_hours.c

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+* print_minutes_of_day - Function that prints every minute of the day
+* from 00:00 to 23:59
+*/
+void print_minutes_of_day(void)
+{
+int hour, minute;
+for (hour = 0; hour < 24; hour++)
+{
+for (minute = 0; minute < 60; minute++)
+{
+_putchar((hour / 10) + '0');
+_putchar((hour % 10) + '0');
+_putchar(':');
+_putchar((minute / 10) + '0');
+_putchar((minute % 10) + '0');
+_putchar('\n');
+}
+}
+}
