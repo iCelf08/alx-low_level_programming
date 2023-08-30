73e5f5722e5161f8b5bded30657f17e432ee5896
chaimaa <chfirstly@gmail.com>
2023-08-30 19:22:57 +0100

    3-factorial.c

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -2,16 +2,16 @@
 /**
  *_strlen_recursion - function that returns the length of a string
  *@s: string to be mmesured
- *Return : integer size of string
+ *Return:integer size of string 0 not success
  */
 int _strlen_recursion(char *s)
 {
 if (*s == '\0')
 {
-return(0);
+return (0);
 }
 else
 {
-return(1 + _strlen_recursion(s + 1));
+return (1 + _strlen_recursion(s + 1));
 }
 }
