a732742905c45c653ad043980eb47c3482b5727c
chaimaa <chfirstly@gmail.com>
2023-09-01 20:42:38 +0100

    mynewnameis

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,7 +1,14 @@
+#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
+/**
+*main - program print's it's name
+*@argc:number arguments,size of argv array
+*@argv:array containing program commande line arguments
+*Return: 0 success
+*/
 int main(int argc, char **argv)
 {
-printf("%s\n",argv[0]);
+printf("%s\n", argv[0]);
 return (0);
 }
