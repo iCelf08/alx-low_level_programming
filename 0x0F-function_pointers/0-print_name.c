30d301b82f485e0975553aeb233d9f3f17554060
chaimaa <chfirstly@gmail.com>
2023-09-13 17:38:07 +0100

    Betty corretion

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -3,7 +3,7 @@
 /**
  * print_name- Function that prints name
  *@name: pointer at name string
- *f:pointer to return the printed name
+ *@f:pointer to return the printed name
  *Return: Nothing
  */
 void print_name(char *name, void(*f)(char *))
