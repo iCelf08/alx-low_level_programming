a0f406f6bee4b01992fb1822706f2b9c801eae48
chaimaa <chfirstly@gmail.com>
2023-09-14 16:50:43 +0100

    switchers

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -11,7 +11,8 @@
 int sum_them_all(const unsigned int n, ...)
 {
 va_list ap;
-unsigned int i, sum = 0;
+unsigned int i;
+unsigned int sum = 0;
 va_start(ap, n);
 for (i = 0; i < n; i++)
 sum += va_arg(ap, int);
