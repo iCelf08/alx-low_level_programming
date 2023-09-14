4019836b65ab51e6988dbb6037ef0decd04151fe
chaimaa <chfirstly@gmail.com>
2023-09-14 12:49:57 +0100

    2-int_index.c

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,17 @@
+#include "function_pointers.h"
+#include <stdio.h>
+/**
+ *array_iterator - printseach array element on a new line
+ *@array: array
+ *@size: size of array that will be printed
+ *@action: pointer to print in regularor hexadecimal
+ *Return: nothing
+ */
+void array_iterator(int *array, size_t size, void((*action),(int))
+{
+unsigned int i;
+if (array == NULL || action == NULL)
+return;
+for (i=0; i < size; i++)
+action(array[i]);
+}
