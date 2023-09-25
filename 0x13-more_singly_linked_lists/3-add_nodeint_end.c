3282440a26d91de3350b5a8f1d4830095d1c8226
iCelf08 <chfirstly@gmail.com>
2023-09-26 00:02:06 +0100

    betty style 3

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -12,8 +12,8 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 listint_t *new_end;
 listint_t *current;
 new_end = malloc(sizeof(listint_t));
-if(new_end == NULL)
-return(NULL);
+if (new_end == NULL)
+return (NULL);
 new_end->n = n;
 new_end->next = NULL;
 if (*head == NULL)
