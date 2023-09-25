f53409db78d22c0c360cbbe28d90c1e3503a67a3
iCelf08 <chfirstly@gmail.com>
2023-09-25 10:55:52 +0100

    0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 #include "lists.h"
 /**
- * print_listint - function that prints all elements of listint_t
- * Return: numeber of element
- */
+* print_listint - function that prints all elements of listint_t
+*@h: head of linked list
+* Return: numeber of element
+*/
 size_t print_listint(const listint_t *h)
 {
 size_t a = 0;
@@ -13,5 +14,5 @@ printf("%d\n", h->n);
 h = h->next;
 a++;
 }
-return(a);
+return (a);
 }
