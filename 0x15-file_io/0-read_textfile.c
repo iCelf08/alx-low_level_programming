3fe36bb50c70f1493174391afd7a2b6126ba6839
iCelf08 <chfirstly@gmail.com>
2023-10-02 17:42:42 +0100

    Re

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -3,28 +3,24 @@
 #include <stdio.h>
 #include "main.h"
 /**
- *read_textfile: function reads text and prints it to posix STO
- *@filename: file name input
- *@letters: letters to print
- *Return: number of letters in success , 0 if failed
- */
+*read_textfile - function reads text and prints it to posix STO
+*@filename: file name input
+*@letters: letters to print
+*Return: number of letters in success , 0 if failed
+*/
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-size_t i;
-char *buffer;
-FILE *pf;
-buffer = malloc(letters * sizeof(char));
-if(buffer == NULL)
-return(0);
-pf = fopen(filename, "r");
-if(pf == NULL)    
-return(0);
-while(fgets(buffer, letters, pf) != NULL)
-{
-for(i = 0; buffer[i] != '\0'; i++)
-_putchar(buffer[i]);
-}
-fclose(pf);
-free(buffer);
-return(letters);
+char *buf;
+ssize_t fd;
+ssize_t w;
+ssize_t t;
+fd = open(filename, O_RDONLY);
+if (fd == -1)
+return (0);
+buf = malloc(sizeof(char) * letters);
+t = read(fd, buf, letters);
+w = write(STDOUT_FILENO, buf, t);
+free(buf);
+close(fd);
+return (w);
 }
