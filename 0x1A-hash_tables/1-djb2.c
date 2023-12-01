038a54eefe9d429fcce0ecb088fca3f505bac7a4
iCelf08 <chfirstly@gmail.com>
2023-12-01 16:54:56 +0100

    1

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -9,10 +9,10 @@
 
 unsigned long int hash_djb2(const unsigned char *str)
 {
-unsigned long int h;
+unsigned long int hash;
 int c;
 hash = 5381;
 while ((c = *str++))
-h = ((hash << 5) + hash) + c;
+hash = ((hash << 5) + hash) + c;
 return (hash);
 }
